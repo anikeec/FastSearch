1797540320b57c1955338268cf82f7e68cd97b2c
apu <pasha_anik@ukr.net>
2017-11-08 20:51:43 +0200

    Program can find files and info inside it

--- src/main/java/com/apu/fastsearch/Main.java
+++ src/main/java/com/apu/fastsearch/Main.java
@@ -5,6 +5,15 @@
  */
 package com.apu.fastsearch;
 
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
@@ -16,31 +25,62 @@ import java.util.concurrent.Executors;
  */
 public class Main {
     
-    private static final int AMOUNT_OF_THREADS = 10;
-    private static final int QUEUE_SIZE = 50;
+    private static final int AMOUNT_OF_THREADS = 20;
+    private static final int QUEUE_SIZE = 20;
     
-    public static void main(String[] args) throws InterruptedException {
-        String path = "D:\\";
-        String strToSearch = "AMEC";
+    public static void main(String[] args) throws InterruptedException, UnsupportedEncodingException {
+        System.out.print("Enter path for search (example: D:\\):  ");
+        Scanner scannerInput = new Scanner(System.in);
+        String path = scannerInput.nextLine();
+        System.out.print("Enter filename or part for search:  ");
+        String fileNameToSearch = scannerInput.nextLine();
+        System.out.print("Enter string or substring for search in files (press enter if search only file names):  ");
+        String strToSearch = scannerInput.nextLine();
+        String resultFileName = "searchResults.txt";        
         
         SearchWorkersCounter swc = new SearchWorkersCounter();
         BlockingQueue<FileObject> queue = new ArrayBlockingQueue(QUEUE_SIZE, true);
         ExecutorService exService = Executors.newFixedThreadPool(AMOUNT_OF_THREADS);
         ConcurrentCounter scannedFilesCounter = new ConcurrentCounter();
-        SearchWorker sw = new SearchWorker(path, strToSearch, queue, exService, swc, scannedFilesCounter);
+        SearchWorker sw = new SearchWorker(path, fileNameToSearch, strToSearch, 
+                                    queue, exService, swc, scannedFilesCounter);
         exService.submit(sw);
         
-        Thread.sleep(100);
+        Thread.sleep(200);
         
+        System.out.println("Start scan filesystem. Wait for results.");
+        List<String> list = new ArrayList<>();
+        FileObject fobj;
         while (true) { 
             if(!queue.isEmpty()) {
-                System.out.println(queue.take().toString());            
+                fobj = queue.take();
+                list.add(fobj.getPath());           
             }
             if(swc.getAmount() == 0)    break;
         }
-        System.out.println(swc.getAmount());
-        System.out.println(scannedFilesCounter.getValue());
+        
+        PrintStream  sourceOutputStream = System.out;
+        
+        try {
+            System.setOut(new PrintStream(new BufferedOutputStream(new FileOutputStream(resultFileName))));
+        } catch (FileNotFoundException ex) {
+            System.setOut(sourceOutputStream);
+            System.out.println("Error - can't write to output file");            
+        }
+
+        System.out.println("List size: " + list.size());
+        System.out.println("Scanned: " + scannedFilesCounter.getValue());
+        System.out.println();
+        
+        Collections.sort(list);
+        for(String str : list) {
+            System.out.println(str);
+        }   
+        
         exService.shutdown();
+        System.setOut(sourceOutputStream);
+        System.out.print("Results stored to file. Press any key to out.");
+        scannerInput.nextLine();
     }
     
 }
--- src/main/java/com/apu/fastsearch/SearchWorker.java
+++ src/main/java/com/apu/fastsearch/SearchWorker.java
@@ -6,6 +6,10 @@
 package com.apu.fastsearch;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.UnsupportedEncodingException;
+import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.logging.Level;
@@ -19,16 +23,18 @@ public class SearchWorker implements Runnable {
     
     private String path;
     private String strToSearch;
+    private String fileNameToSearch;
     private BlockingQueue<FileObject> resultQueue;
     private ExecutorService exService;
     private SearchWorkersCounter swc;
     private ConcurrentCounter scannedFilesCounter;
 
-    public SearchWorker(String path, String strToSearch,  
+    public SearchWorker(String path, String fileNameToSearch, String strToSearch,   
                 BlockingQueue<FileObject> resultQueue, ExecutorService exService, 
                 SearchWorkersCounter swc, ConcurrentCounter counter) {
         this.path = path;
         this.strToSearch = strToSearch;
+        this.fileNameToSearch = fileNameToSearch;
         this.resultQueue = resultQueue;
         this.exService = exService;
         this.swc = swc;
@@ -40,31 +46,60 @@ public class SearchWorker implements Runnable {
         try {
             swc.amountInc();
             scannedFilesCounter.intValue();
-            findName();
+            find();
         } catch (InterruptedException ex) {
+            Logger.getLogger(SearchWorker.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedEncodingException ex) {
             Logger.getLogger(SearchWorker.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             swc.amountDec();
         }         
     }
     
-    private void findName() throws InterruptedException {
+    private void find() throws InterruptedException, UnsupportedEncodingException {
         File file = new File(path);
         FileObject fo = new FileObject(file);        
         if(file.exists()) {
             //compare file or directory name
-            if(file.getName().toUpperCase().contains(strToSearch.toUpperCase())) {
+            if(file.getName().toUpperCase().contains(fileNameToSearch.toUpperCase())) {                
+                if((strToSearch.equals(""))&& (file.isFile())) { 
+                    resultQueue.put(fo);
+                    return;
+                }               
+                if(file.isFile()) { 
+                    String str;                    
+                    try {
+//                        if(file.length() > 100000000) return;
+                        FileReader fReader = new FileReader(file);
+                        Scanner scanner = new Scanner(fReader);
+                        while(scanner.hasNext()){
+                            str = new String(scanner.nextLine().getBytes(), "Cp1251");//UTF-8
+                            if(str.toUpperCase()
+                                        .contains(strToSearch.toUpperCase())) {
+                                String pathTemp = fo.getPath() + " - " + str;
+                                fo.setPath(pathTemp);
+                                resultQueue.put(fo);
+                                return;
+                            }
+                        }
+                    } catch (FileNotFoundException ex) {
+                        String pathTemp = fo.getPath() + " - Error open file!!!";
+                        fo.setPath(pathTemp);
+                        resultQueue.put(fo);
+                    }
+                    return;
+                }
                 resultQueue.put(fo);
-            }
-            if(file.isFile()) {                
+            } 
+            if(file.isFile()) { 
                 return;
             }
             //search start
             File[] files = file.listFiles();
             SearchWorker sw;
             for(File fl : files) {
-                sw = new SearchWorker(fl.getPath(), strToSearch, resultQueue, 
-                                        exService, swc, scannedFilesCounter);
+                sw = new SearchWorker(fl.getPath(), fileNameToSearch, strToSearch,  
+                            resultQueue, exService, swc, scannedFilesCounter);
                 exService.submit(sw);
             }
         }        
